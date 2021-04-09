b6952efc3c98c21d897ac384866f5e2073e9ab43
jwhangbo <35470132+jwhangbo@users.noreply.github.com>
2021-04-09 13:46:27 -0700

    added MongoDB and Mongoose

--- bakuretsu/app.js
+++ bakuretsu/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const mongoose = require('mongoose')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,6 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// mongoose setup
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('mongoose connected')
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
