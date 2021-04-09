14a9966fc5ffd9bb178fd545148b466cfc1358ed
jwhangbo <35470132+jwhangbo@users.noreply.github.com>
2021-04-09 14:48:46 -0700

    added file upload handling

--- bakuretsu/routes/index.js
+++ bakuretsu/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* POST fileupload */
+router.post('/fileupload', (req, res, next) => {
+  const form = formidable({ multiples: true });
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json({ fields, files });
+  });
 });
 
 module.exports = router;
