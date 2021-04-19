44570aeaa52dbd8b7e9538b40fa3532cbb498252
Michael Zhao <mweiyuzhao@gmail.com>
2021-04-19 00:36:05 -0400

    Added storing uploaded file metadata to mongodb and hashing

--- bakuretsu/routes/index.js
+++ bakuretsu/routes/index.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var formidable = require('formidable');
+var crypto = require('crypto');
+
+const mongoose = require('mongoose')
+
+// schema setup
+
+const ImageData = mongoose.model('ImageData',
+  new mongoose.Schema({
+    data: {
+      path: String,
+      name: String,
+      time: Date
+    },
+    hash: String
+}));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,7 +31,29 @@ router.post('/fileupload', (req, res, next) => {
       next(err);
       return;
     }
-    res.json({ fields, files });
+    var d = {
+      path: files.fileupload.path,
+      name: files.fileupload.name,
+      time: Date.now()
+    }
+    if (!d.name) {
+      next(console.log("Error file has no name"));
+      return;
+    }
+    var md5sum = crypto.createHash('md5');
+    md5sum.update(JSON.stringify(d));
+    var h = md5sum.digest();
+    img = new ImageData({ data: d, hash: h });
+    console.log(img);
+    img.markModified();
+    img.save(function (err, img) {
+      if (err) {
+        next(err);
+        return;
+      }
+    });
+    result = { data: d, hash: h.toString('hex') };
+    res.json(result);
   });
 });
 
